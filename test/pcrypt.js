d65d1a57353d9edd7e1290c4cffe38fd47a323bf
Jake Luer <jake@alogicalparadox.com>
2013-08-13 19:07:41 +0300

    tests: refactor for node 8/10

--- test/pcrypt.js
+++ test/pcrypt.js
@@ -1,114 +1,61 @@
-var pcrypt = require('..')
+var Pcrypt = require('..')
   , pass = 'hello-universe'
   , notpass = 'hello-world';
 
-describe('generate', function () {
-
-  it('can make a passkey', function (done) {
-    pcrypt.gen(pass, function (err, key) {
-      Should.not.exist(err);
-      key.should.be.a('string')
-        .with.length(300);
-      done();
-    });
-  });
-
-});
-
-describe('compare', function () {
-
-  it('can matches on same pass', function (done) {
-    pcrypt.gen(pass, function (err, str) {
-      Should.not.exist(err);
-      pcrypt.compare(pass, str, function (err, match) {
-        Should.not.exist(err);
-        match.should.be.true;
+describe('Pcrypt', function () {
+  describe('.gen(pass, cb)', function () {
+    it('can make a passkey', function (done) {
+      var pcrypt = Pcrypt();
+      pcrypt.gen(pass, function (err, key) {
+        should.not.exist(err);
+        key.should.be.a('string')
+          .with.length.above(250);
         done();
       });
     });
   });
 
-  it('doesn\'t match on different pass', function (done) {
-    pcrypt.gen(pass, function (err, str) {
-      Should.not.exist(err);
-      pcrypt.compare(notpass, str, function (err, match) {
-        Should.not.exist(err);
-        match.should.be.false;
-        done();
+  describe('.compare(pass, str, cb)', function () {
+    it('can match on same pass', function (done) {
+      var pcrypt = Pcrypt();
+      pcrypt.gen(pass, function (err, str) {
+        should.not.exist(err);
+        pcrypt.compare(pass, str, function (err, match) {
+          should.not.exist(err);
+          match.should.be.true;
+          done();
+        });
+      });
+    });
+
+    it('can not match on different pass', function (done) {
+      var pcrypt = Pcrypt();
+      pcrypt.gen(pass, function (err, str) {
+        should.not.exist(err);
+        pcrypt.compare(notpass, str, function (err, match) {
+          should.not.exist(err);
+          match.should.be.false;
+          done();
+        });
       });
     });
   });
 
-});
+  describe('.random()', function () {
+    it('can generate a random password', function (done) {
+      var pcrypt = Pcrypt();
+      pcrypt.random(function (err, pass, key) {
+        should.not.exist(err);
+        should.exist(pass);
+        should.exist(key);
 
-describe('length', function () {
-
-  it('can set length', function (done) {
-    pcrypt.length = 256;
-    pcrypt.should.have.length(256);
-
-    pcrypt.gen(pass, function (err, key) {
-      Should.not.exist(err);
-      key.should.be.a('string')
-        .with.length(600);
-      pcrypt.length = 128;
-      done();
-    });
-  });
-
-});
-
-describe('iterations', function () {
-
-  it('can set length', function (done) {
-    pcrypt.iterations = 2000;
-    pcrypt.iterations.should.equal(2000);
-
-    pcrypt.gen(pass, function (err, key) {
-      Should.not.exist(err);
-      key.should.be.a('string')
-        .with.length(300);
-      pcrypt.iterations = 1000;
-      done();
-    });
-  });
-
-});
-
-describe('random password generation', function () {
-  var charsDef = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    , charsNew = 'abc123';
-
-  beforeEach(function () {
-    pcrypt.randChars = charsDef;
-    pcrypt.randLength = 10;
-  });
-
-  it('can be configured', function () {
-    pcrypt.should.have.property('randChars', charsDef);
-    pcrypt.should.have.property('randLength', 10);
-
-    pcrypt.randChars = charsNew;
-    pcrypt.should.have.property('randChars', charsNew);
-
-    pcrypt.randLength = 20;
-    pcrypt.should.have.property('randLength', 20);
-  });
-
-  it('can generate a random password', function (done) {
-    pcrypt.random(function (err, pass, key) {
-      Should.not.exist(err);
-      Should.exist(pass);
-      Should.exist(key);
-
-      pass.should.have.length(pcrypt.randLength);
-
-      pcrypt.compare(pass, key, function (err, match) {
-        Should.not.exist(err);
-        match.should.be.true;
-        done();
+        pass.should.have.length(pcrypt.get('rand length'));
+        pcrypt.compare(pass, key, function (err, match) {
+          should.not.exist(err);
+          match.should.be.true;
+          done();
+        });
       });
     });
   });
-
 });
